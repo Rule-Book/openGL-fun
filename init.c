fcce84f89283afb0f51ebb17194269816dcd8dbd
Zachary Solomon <trunkz77@live.com>
2023-10-23 16:55:43 -0400

    copy linmath.h from glfw src folder to MinGW/include

--- init.c
+++ init.c
@@ -1,10 +1,43 @@
-#include <glad/glad.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+#include "linmath.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 
 //code to compile g++ init.c -lglfw3 -lopengl32 -lgdi32
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+ 
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
 
 void error_callback(int error, const char* description)
 {
@@ -19,6 +52,10 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main(void)
 {
+	GLFWwindow* window;
+	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+	GLint mvp_location, vpos_location, vcol_location;
+
 	glfwSetErrorCallback(error_callback);
 
 	if (!glfwInit())
@@ -27,7 +64,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Simple Example", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Simple Example", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -37,6 +74,7 @@ int main(void)
 	glfwSetKeyCallback(window, key_callback);
 
 	glfwMakeContextCurrent(window);
+	gladLoadGL(glfwGetProcAddress);
 	glfwSwapInterval(1);
 
 	glGenBuffers(1, &vertex_buffer);
